6aeafe232eac4489f7446af7c4eb6c9b53ced4b3
Herrhik <hrabe@bocasay.fr>
2016-03-31 16:31:44 +0300

    Modification evenement on click

--- map/map.js
+++ map/map.js
@@ -1,7 +1,69 @@
 (function($){
 	$('polygon').each(function(){
-		$(this).on('click',function(){
-			console.log( $(this).data('num') )
-		});
+		
+		var tmp = $(this).attr('fill');
+		var codePays = $(this).data('num');
+
+		// Check the blue zone only
+		if (tmp != '#CCCCCB'){
+
+			// Change fill color
+			$(this).on('mouseenter',function(){			
+				$(this).attr('fill','#ff0000');
+			});
+			$(this).on('mouseleave',function(){
+				$(this).attr('fill',tmp);
+			});
+
+			// Show the partner depending on the country		
+			$(this).on('click',function(){
+				$('.detail-list').html('');
+				$('.detail-list').html('<li>'+ getCountry(codePays) +'</li>');
+			});
+		}		
 	});
+
+	function getCountry(code){
+		var country = '';
+		switch (code){
+			case 'FI':
+				country = 'Finlande';
+				break;
+			case 'GB':
+				country = 'Royaume Uni';
+				break;
+			case 'BE':
+				country = 'Belgique';
+				break;
+			case 'LU':
+				country = 'Luxembourg';
+				break;
+			case 'FR':
+				country = 'France';
+				break;
+			case 'SW':
+				country = 'Suisse';
+				break;
+			case 'IT':
+				country = 'Italie';
+				break;
+			case 'PT':
+				country = 'Portugal';
+				break;
+			case 'SI':
+				country = 'Sicile';
+				break;
+			case 'SA':
+				country = 'Sardaigne';
+				break;
+			case 'ES':
+				country = 'Espagne';
+				break;
+			case 'CO':
+				country = 'Corse';
+				break;
+		}
+		return country;
+	}
+
 }) (jQuery);
